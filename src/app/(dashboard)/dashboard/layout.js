65bd95d953a2d9224258bcbbcc4398e4289c6941
msozkan90 <msozkan90@gmail.com>
2026-07-17 21:32:16 +0300

    feat: add ScrollToTopButton component and integrate it into DashboardLayout; update translations for back to top action

--- src/app/(dashboard)/dashboard/layout.js
+++ src/app/(dashboard)/dashboard/layout.js
@@ -4,8 +4,10 @@ import { Layout, theme } from "antd";
 import HeaderBar from "@/components/layout/HeaderBar";
 import Sidebar from "@/components/layout/Sidebar";
 import PageContainer from "@/components/layout/PageContainer";
+import ScrollToTopButton from "@/components/layout/ScrollToTopButton";
 import { useSelector } from "react-redux";
 import { usePathname } from "next/navigation";
+import { useRef } from "react";
 
 const { Content } = Layout;
 
@@ -13,6 +15,7 @@ export default function DashboardLayout({ children }) {
   const collapsed = useSelector((s) => s.ui.sidebarCollapsed);
   const pathname = usePathname();
   const { token } = theme.useToken();
+  const contentRef = useRef(null);
 
   return (
     <Layout hasSider style={{ height: "100vh" }}>
@@ -22,6 +25,7 @@ export default function DashboardLayout({ children }) {
       >
         <HeaderBar pathname={pathname} />
         <Content
+          ref={contentRef}
           style={{
             minHeight: 0,
             flex: 1,
@@ -32,6 +36,7 @@ export default function DashboardLayout({ children }) {
         >
           <PageContainer>{children}</PageContainer>
         </Content>
+        <ScrollToTopButton targetRef={contentRef} />
       </Layout>
     </Layout>
   );
